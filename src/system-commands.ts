2aa099c93f70830cb12803fd06212b0a0f3a3c16
Tobias S. Keller <t.keller@blink.it>
2025-03-20 23:06:17 +0100

    Update system commands for improved feedback and user experience
    - Enhanced the `restart-device` and `restart-server` commands to include echo statements for better user feedback during execution.
    - Modified the `update-software` command to run as the `keller` user, ensuring proper permissions for software updates.
    - Included comments and doc blocks for improved code clarity and maintainability.

--- src/system-commands.ts
+++ src/system-commands.ts
@@ -1,8 +1,9 @@
 export const systemCommands = {
-  "restart-device": "sudo reboot",
-  "restart-server": "sudo systemctl restart lightstrip.service",
+  "restart-device": "echo 'Rebooting device...' && sleep 1 && sudo reboot",
+  "restart-server":
+    "echo 'Restarting server...' && sleep 1 && sudo systemctl restart lightstrip.service",
   "update-software":
-    "cd /home/keller/repos/lightmonitorstrip && git pull && npm run build && echo 'Software updated successfully - Please restart the server'",
+    "cd /home/keller/repos/lightmonitorstrip && sudo -u keller git pull && sudo -u keller npm run build && echo 'Software updated successfully - Please restart the server'",
   "flash-firmware":
     "cd /home/keller/repos/lightmonitorstrip/hardware/firmware && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:leonardo .",
 };
